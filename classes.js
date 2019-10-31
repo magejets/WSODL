672ab55393015dcc754da922c1e0fac1d8d4ce87
mage j lewis <mage.j.lewis@gmail.com>
2019-10-31 15:03:32 -0600

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,158 @@
+//Sound Class
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.setAttribute("loop","loop");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+//Block Class
+function block(x,y,width,height,material){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.material = material;
+
+    this.draw = function(){
+        //console.log("drawing block");
+        ctx.beginPath();
+        ctx.rect(x-cameraX,y-cameraY,width,height);
+        if(x==0){
+            ctx.fillStyle = "#003363";
+        }else if(material == "solid"){
+            ctx.fillStyle = "#eec666";
+        }else if(material == "glass"){
+            ctx.fillStyle = "#ddeeff";
+        }
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function skiier(x,y,width,height,skiLength,color){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.skiLength = skiLength;
+    this.color = color;
+    this.speedY = 0;
+    this.speedx = 1;
+    this.jumpKey = 0;
+    this.jumpSpeed = 10;
+    this.dead = false;
+
+    this.draw = function(){
+        //console.log("drawing");
+        ctx.beginPath();
+        ctx.rect(this.x,this.y,this.width,this.height);
+        ctx.rect((this.x+(this.width/2))-(this.skiLength/2),(this.y+this.height)-5,this.skiLength,5);
+        ctx.moveTo(this.x+this.width,this.y+this.height/2);
+        ctx.lineTo(stage.width,stage.height/3);
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    this.walk = function(speed){
+        this.x += speed;
+        var slope;
+        for(var i=0; i<map.length;i++){
+            var p = map[i];
+            slope = 0;
+            while(slope < 8 && collisionDetection(this,p) == true){
+                this.y -= 1;
+                slope +=1;
+            }
+                
+            if(slope >= 8){
+                this.x -= speed;
+                this.y += slope;
+                resetVars();
+            }
+        }
+    }
+
+    this.touchGround = function(up){
+        this.falling += 1;
+        for(var i =0; i<map.length;i++){
+            var p = map[i];
+        
+            while(collisionDetection(this,p)==true){
+                if(up){
+                    this.y += .5
+                }else {
+                    this.y -= .5; //this number must be equal to what speedY changes each time
+                    this.falling = 0;
+                }
+                this.speedY = 0;
+
+            }
+        }
+    }
+
+    this.kill = function(){
+        started = false;
+        this.dead = true;
+    }
+}
+
+function Boomerang(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 15;
+    this.height = 15;
+    this.direction = 1;
+    this.speed = 5;
+    this.moving = false;
+
+    this.draw = function (){
+        ctx.beginPath();
+        if(Math.floor(this.x)%2 == 0){
+            ctx.rect(this.x-cameraX,this.y-cameraY,5,15);
+            ctx.rect(this.x-cameraX,(this.y-cameraY)+10,15,5);
+        }else{
+            ctx.rect(this.x-cameraX,this.y-cameraY,5,15);
+            ctx.rect(this.x-cameraX,this.y-cameraY,15,5);
+        }
+        ctx.fillStyle = "#f96717";
+        ctx.fill();
+        ctx.closePath();
+    }
+    this.update = function(skiier){
+        console.log(this.direction);
+        if(this.moving){
+            console.log(skiier.x);
+            this.x += this.direction*this.speed;
+            if(Math.abs(this.x-realX)>150){
+                this.direction*=-1;
+            }
+            if(Math.abs(this.x-realX)<15 && this.direction == -1){
+                this.direction = 1;
+                this.moving = false;
+            }
+            if(this.direction == -1){
+                if(this.y<realY){
+                    this.y+=1;
+                }else{
+                    this.y-=1;
+                }
+            }
+        }else{
+            this.x = realX;
+            this.y = realY;
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,210 @@
+/****************************************
+*      COPYRIGHT 2019 MAGE J LEWIS      *
+****************************************/
+
+//Canvas setup
+var stage = document.getElementById("stage");
+var ctx = stage.getContext("2d");
+
+//initialize variables
+var time;
+var music = new sound("backgroundTunes.mp3");
+var started = false;
+var ended = false;
+var map = [];
+var lastBlock;
+var player;
+var boomerang;
+var gravity;
+var cameraX;
+var cameraY;
+var realX = stage.width/4;
+var realY = stage.height/2;
+
+//Keyboard vars
+var up = false;
+var down = false;
+var left = false;
+var right = false;
+var space = false;
+
+//Resets the vars for every time you die
+function resetVars(){
+    time = 0;
+    map = [];  
+    player = new skiier(stage.width/4,stage.height/2,20,30,50,"#770099");
+    boomerang = new Boomerang(-30,-30);
+    music.sound.currentTime = 0;
+    music.sound.play();
+    started = true;
+    ended = false;
+    map.push(new block(0,280,240,500,"solid"));
+    map.push(new block(240,280,100,500,"solid"));
+    lastBlock = map[1];
+    player.jumpSpeed = 30;
+    gravity = 0.5;
+    cameraX = 0;
+    cameraY = 0;
+}
+
+//Keyboard functions
+function keyDownHandler(e){
+    if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w"){
+        up = true;
+    }
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d"){
+        right = true;
+    }
+    if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a"){
+        left = true;
+    }
+    if(e.key == " "){
+        space = true;
+    }
+}
+function keyUpHandler(e){
+    if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w"){
+        up = false;
+    }
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d"){
+        right = false;
+    }
+    if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a"){
+        left = false;
+    }
+    if(e.key == " " /*space bar*/){
+        space = false;
+    }
+}
+
+//Takes in two objects with width, height, x and y and determines if they overlap
+function collisionDetection(body1,body2){
+    return body1.x+body1.width > body2.x-cameraX && body1.x < body2.x+body2.width-cameraX && 
+           body1.y+body1.height > body2.y-cameraY && body1.y < body2.y+body2.height-cameraY
+}
+
+//Returns a random int from the min to the max. Includes min but not max
+function randInt(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+//returns a color for the sky based on how long the player has been going
+function getSkyColor(t){
+    var x = 60*((t/60)-Math.floor(t/60)); //x = time of day. t = total time since start of round
+    var light = ((-17/60)*x*x)+17*x;
+    var color = "rgb("+light/3+","+light/1.2+","+light+")";
+    ctx.beginPath();
+    ctx.rect(x*8,320-light*1.2,50,50);
+    ctx.fillStyle = "#f4c22e";
+    ctx.fill();
+    ctx.closePath();
+    return color;
+}
+
+//draws the ground
+function drawMap(){
+    for(var i= 0; i<map.length; i++){
+        map[i].draw();
+    }
+}
+
+//Detects when the boomerang collides with a block and responds accordingly
+function boomyCollisions(){
+    for(var i = 0; i < map.length; i++){
+        if(boomerang.x+boomerang.width-cameraX > map[i].x-cameraX && boomerang.x-cameraX < map[i].x+map[i].width-cameraX && 
+           boomerang.y+boomerang.height-cameraY > map[i].y-cameraY && boomerang.y-cameraY < map[i].y+map[i].height-cameraY){
+            console.log("wakonda");
+            boomerang.direction = -1;
+            if(map[i].material == "glass"){
+                map.splice(i,1);
+            }
+        }
+    }
+}
+
+
+//Generates the random terrain
+function makeMap(){
+    var maxX = lastBlock.x+lastBlock.width
+    while(realX+480>maxX){
+        var xBase = lastBlock.x+lastBlock.width;
+        var x = xBase+randInt(1,50+Math.floor(time/100));
+        var yMax = ((player.jumpSpeed*player.jumpSpeed)/(4*gravity))*(0.1+time/500);
+        var y = lastBlock.y+randInt(-1*yMax,.5*yMax);
+        var width = randInt(25,200);
+        var height = 500;
+        //console.log(yMax);
+        var material = "solid";
+        map.push(new block(x,y,width,height,material));
+        if(randInt(1,15)==10){
+            map.push(new block(x+((width)/2),y-100,20,100,"glass"));
+        }
+        lastBlock = new block(x,y,width,height,material);
+        maxX = lastBlock.x+lastBlock.width
+    }
+}
+
+//Moves the camera based on where the player is
+function updateCamera(x,y){
+    cameraX += x-stage.width/4;
+    cameraY += y-stage.height/2;
+}
+
+//Main Game loop
+function main(){
+if(started){    //Don't start until the start button is clicked
+    ctx.clearRect(0,0,stage.width,stage.height);
+    document.getElementById("stage").style.background = getSkyColor(time);
+    ctx.font = "17px Ariel";
+    ctx.fillStyle = "#FFFFff";
+    ctx.fillText("Time: "+Math.floor(time),10,20);
+    time +=0.01;
+    makeMap();
+    player.speedX = 2;
+    if(Math.abs(player.speedX)>0.2){
+        player.walk(player.speedX);
+    }
+    if(up == true){
+        if(player.falling<3 && player.jumpKey==0){
+            player.speedY = 10;
+            player.jumpKey = 1;
+        }
+    } else {
+        player.jumpKey = 0;
+    }
+    player.y -= player.speedY;
+    if(player.speedY<4 || up== true){
+        player.speedY -= gravity;
+    } else {
+        player.speedY -= gravity*2;
+    }
+    if(right && !boomerang.moving){
+        boomerang.moving = true;
+    }
+    player.touchGround(player.speedY>0);
+    updateCamera(player.x,player.y);
+    player.x = stage.width/4;
+    player.y = stage.height/2;
+    realX = cameraX+player.x;
+    realY = cameraY+player.y;
+    boomerang.update(player);
+    boomyCollisions();
+    boomerang.draw();
+    player.draw();
+    drawMap();
+    if(player.dead){
+        ctx.clearRect(0,0,stage.width,stage.height);
+        ctx.font = "69px Ariel";
+        ctx.fillStyle = "#ff0000";
+        ctx.fillText("YOU DIED!", 50,stage.height/2);      
+        music.sound.pause();
+    }
+}
+}
+
+//Keyboard event listeners
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//Make the game loop loop
+setInterval(main,10);
